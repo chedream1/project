f1cbee89e97a7c3a66a1533322004ebf164b18fa
chedream1 <144874595+chedream1@users.noreply.github.com>
2023-09-13 12:48:34 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,7 @@
+input.onPinPressed(TouchPin.P0, function () {
+    if (true) {
+    	
+    } else {
+        music.play(music.stringPlayable("E B C5 A B G A F ", 120), music.PlaybackMode.LoopingInBackground)
+    }
+})
